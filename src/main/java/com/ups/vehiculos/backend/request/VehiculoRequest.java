26e2142eeb7e884753efbbb9c65b2e0a286a1963
Juan Naula <j.naulas@jardinazuayo.fin.ec>
2023-09-20 11:38:20 -0500

    Se crea Controlar y Request para pruebas

---
+++ src/main/java/com/ups/vehiculos/backend/request/VehiculoRequest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.ups.vehiculos.backend.request;
+
+/**
+ * 
+ */
+public class VehiculoRequest {
+	private long id;
+	private int ano;
+	private int cilindraje;
+	private String marca;
+	
+	public long getId() {
+		return id;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public int getCilindraje() {
+		return cilindraje;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	
+
+}
